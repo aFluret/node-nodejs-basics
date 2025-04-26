b2ad4138da4ae021f1ec474721173441223a74fb
Fluret <akuntsievich@inbox.ru>
2025-04-26 21:31:58 +0300

    create.js implemetantion

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
-const create = async () => {
-    // Write your code here 
+import fs,{exists} from 'fs'
+const create =  () => {
+    
+    exists('fresh.txt', (exists) => {
+        if (exists) { throw new Error("FS operation failed")};
+    });
+    const content = 'I am fresh and young';
+
+    fs.writeFile('fresh.txt', content, err => {
+        if (err) {
+            console.error(err);
+        } else {
+            // file written successfully
+        }
+    });
+    
+
 };
 
-await create();
+create();
